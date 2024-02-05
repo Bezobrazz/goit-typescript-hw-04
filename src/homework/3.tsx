4571c3eea56364cd153294c61a4681d3f5228d96
Viacheslav Kostenko <kostenko.uvd@gmail.com>
2024-02-05 14:24:12 +0200

    task 3 done

--- src/homework/3.tsx
+++ src/homework/3.tsx
@@ -1,13 +1,13 @@
-import React, { useState } from 'react';
+import React, { SetStateAction, useState } from "react";
 
 export function FormComponent() {
-  const [value, setValue] = useState("");
+  const [value, setValue] = useState<string>("");
 
-  const handleChange = (event) => {
+  const handleChange = (event: {
+    target: { value: SetStateAction<string> };
+  }) => {
     setValue(event.target.value);
   };
 
-  return (
-    <input type="text" value={value} onChange={handleChange} />
-  );
+  return <input type="text" value={value} onChange={handleChange} />;
 }
